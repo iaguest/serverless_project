d61514baca6ea0e6c35808a766e9387a4ef7d141
ianguest <iag.1.apps@gmail.com>
2021-02-23 23:53:10 +0000

    simplify previous

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import { getUserId } from '../utils'
-import { getUploadUrl } from '../../s3/attachmentsAccess'
+import { getUploadUrl } from '../../dataLayer/attachmentsAccess'
 import { onGenerateUploadUrl } from '../../businessLogic/todos'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
