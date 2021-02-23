0bcb8b0fbf1bd5b43c36155e140f8b8c3ddfd6a4
ianguest <iag.1.apps@gmail.com>
2021-02-23 23:49:18 +0000

    pull s3 related stuff out into separate layer

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,17 +1,10 @@
 import 'source-map-support/register'
-import * as AWS  from 'aws-sdk'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 import { getUserId } from '../utils'
+import { getUploadUrl } from '../../s3/attachmentsAccess'
 import { onGenerateUploadUrl } from '../../businessLogic/todos'
-
-const s3 = new AWS.S3({
-  signatureVersion: 'v4'
-})
-
-const bucketName = process.env.ATTACHMENTS_S3_BUCKET
-const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
@@ -47,12 +40,4 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       uploadUrl: url
     })
   }
-}
-
-function getUploadUrl(todoId: string) {
-  return s3.getSignedUrl('putObject', {
-    Bucket: bucketName,
-    Key: todoId,
-    Expires: urlExpiration
-  })
 }
---
+++ backend/src/s3/attachmentsAccess.ts
@@ -0,0 +1,16 @@
+import * as AWS  from 'aws-sdk'
+
+const s3 = new AWS.S3({
+    signatureVersion: 'v4'
+})
+  
+const bucketName = process.env.ATTACHMENTS_S3_BUCKET
+const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
+
+export function getUploadUrl(todoId: string) {
+    return s3.getSignedUrl('putObject', {
+        Bucket: bucketName,
+        Key: todoId,
+        Expires: urlExpiration
+    })
+}
