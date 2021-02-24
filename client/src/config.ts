d1b938b4c269b294b4828871cc6c92b5c2936085
ianguest <iag.1.apps@gmail.com>
2021-02-24 21:53:38 +0000

    update client config file

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = ''
+const apiId = 'p1zdio2js8'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-west-2.amazonaws.com/dev`
 
 export const authConfig = {
